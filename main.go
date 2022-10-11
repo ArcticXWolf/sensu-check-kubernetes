285ea5f406802f3584381e175287b1896f2e7b31
Jan Niklas Richter <git@janniklasrichter.de>
2022-10-11 15:19:55 +0200

    Add kubernetes library

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/sensu/sensu-go/types"
 	"github.com/sensu/sensu-plugin-sdk/sensu"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // Config represents the check plugin config.
 type Config struct {
 	sensu.PluginConfig
+	Namespace string
 }
 
 var (
@@ -21,7 +26,17 @@ var (
 		},
 	}
 
-	options = []sensu.ConfigOption{}
+	options = []sensu.ConfigOption{
+		&sensu.PluginConfigOption[string]{
+			Path:      "namespace",
+			Env:       "",
+			Argument:  "namespace",
+			Shorthand: "n",
+			Default:   "",
+			Usage:     "Name of the namespace to query from (leave empty to check clusterwide)",
+			Value:     &plugin.Namespace,
+		},
+	}
 )
 
 func main() {
@@ -35,5 +50,23 @@ func checkArgs(event *types.Event) (int, error) {
 
 func executeCheck(event *types.Event) (int, error) {
 	fmt.Println("First test of asset, it works! (hopefully)")
+
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientset.CoreV1().Pods(plugin.Namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods\n", len(pods.Items))
+
 	return sensu.CheckStateOK, nil
 }
